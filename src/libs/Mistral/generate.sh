666cd56395ba675701d979f34df5d6e7176ed74c
Konstantin S. <havendv@gmail.com>
2026-03-16 21:55:59 +0400

    Remove FixOpenApiSpec helper

--- src/libs/Mistral/generate.sh
+++ src/libs/Mistral/generate.sh
@@ -1,12 +1,7 @@
 dotnet tool install --global autosdk.cli --prerelease
 rm -rf Generated
 curl -o openapi.yaml https://raw.githubusercontent.com/mistralai/platform-docs-public/refs/heads/main/openapi.yaml
-sed -i "s/it's/its/g" openapi.yaml
-dotnet run --project ../../helpers/FixOpenApiSpec openapi.yaml
-if [ $? -ne 0 ]; then
- echo "Failed, exiting..."
- exit 1
-fi
+perl -0pi -e "s/it's/its/g" openapi.yaml
 autosdk generate openapi.yaml \
   --namespace Mistral \
   --clientClassName MistralClient \
